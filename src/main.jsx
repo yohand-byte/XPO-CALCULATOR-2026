2b5958b6d7fc0b239f0a0c9ab1bfb48d80831014
henri <henri@snatch-hub.com>
2026-03-04 08:51:56 +0100

    🚀 XPO Calculator 2026

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import XPOCalculator from './XPOCalculator.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <XPOCalculator />
+  </React.StrictMode>,
+)
